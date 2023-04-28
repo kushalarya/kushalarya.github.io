9148357f4a2cccb16ed1c8e92db265d79296ce1e
Arya <kushalarya999@gmail.com>
2023-04-29 02:13:13 +0530

    adding config changes

--- kushal-portfolio/docusaurus.config.js
+++ kushal-portfolio/docusaurus.config.js
@@ -18,8 +18,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kushalarya', // Usually your GitHub org/user name.
+  projectName: 'kushalarya.github.com', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -76,7 +78,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
