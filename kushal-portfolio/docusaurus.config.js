8f50ff5d6084cb683c3eacc550bd8c8d81ee042d
Arya <kushalarya999@gmail.com>
2023-04-29 02:23:45 +0530

    correcting project name

--- kushal-portfolio/docusaurus.config.js
+++ kushal-portfolio/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kushalarya', // Usually your GitHub org/user name.
-  projectName: 'kushalarya.github.com', // Usually your repo name.
+  projectName: 'kushalarya.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
